05b44e8d1183e77be1ba9fbf9de7057f31f9e5ad
abangdsm <id.reversal@gmail.com>
2023-01-20 16:09:07 +0700

    Mengubah jumlah frame

--- script.js
+++ script.js
@@ -7,10 +7,10 @@ const CANVAS_HEIGHT = canvas.height = 600;
 const playerImage = new Image();
 playerImage.src = 'shadow_dog.png';
 
-const sw = 575;
-const sh = 523;
+const spriteWidth = 575;
+const spriteHeight = 523;
 let frameX = 0;
-let frameY = 0;
+let frameY = 1;
 let gameFrame = 0;
 const staggerFrames = 5;
 
@@ -18,12 +18,19 @@ function animate() {
     ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
     // ctx.fillRect(50, 50, 100, 100);
     // ctx.drawImage(image, sx, sy, sw, sh, dx, dy, dw, dh);
-    ctx.drawImage(playerImage, frameX * sw, frameY * sh, sw, sh, 0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
 
-    if(gameFrame % staggerFrames == 0){
-        if(frameX < 6) frameX++;
-        else frameX = 0;
-    }
+    // Method yang lebih mudah dimulai dari sini
+    let position = Math.floor(gameFrame/staggerFrames) % 6;
+    frameX = spriteWidth * position;
+
+    ctx.drawImage(playerImage, frameX, frameY * spriteHeight, spriteWidth, spriteHeight, 0, 0, spriteWidth, spriteHeight);
+
+
+    // coba dengan method yang lebih mudah
+    // if(gameFrame % staggerFrames == 0){
+    //     if(frameX < 6) frameX++;
+    //     else frameX = 0;
+    // }
 
     gameFrame++;
     requestAnimationFrame(animate);
