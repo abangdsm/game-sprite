12e9a9b9d39a14dbd07c14ad74c71999db8d81b7
abangdsm <id.reversal@gmail.com>
2023-01-20 11:35:12 +0700

    First Commit

---
+++ script.js
@@ -0,0 +1,31 @@
+const canvas = document.getElementById('canvas1');
+const ctx = canvas.getContext('2d');
+
+const CANVAS_WIDTH = canvas.width = 600;
+const CANVAS_HEIGHT = canvas.height = 600;
+
+const playerImage = new Image();
+playerImage.src = 'shadow_dog.png';
+
+const sw = 575;
+const sh = 523;
+let frameX = 0;
+let frameY = 0;
+let gameFrame = 0;
+const staggerFrames = 5;
+
+function animate() {
+    ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+    // ctx.fillRect(50, 50, 100, 100);
+    // ctx.drawImage(image, sx, sy, sw, sh, dx, dy, dw, dh);
+    ctx.drawImage(playerImage, frameX * sw, frameY * sh, sw, sh, 0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+
+    if(gameFrame % staggerFrames == 0){
+        if(frameX < 6) frameX++;
+        else frameX = 0;
+    }
+
+    gameFrame++;
+    requestAnimationFrame(animate);
+}
+animate();
